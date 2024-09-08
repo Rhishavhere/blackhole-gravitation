8628dd9ea3b9151b2047b109b298e3fbac023cdb
Rhishavhere <rhishh.me@gmail.com>
2024-09-09 01:44:01 +0530

    velocity varied color added

--- utils/color.py
+++ utils/color.py
@@ -23,3 +23,27 @@ def get_color_based_on_force(force, max_force):
     b = max(0, min(255, b))
     
     return (r, g, b)
+
+
+def get_color_based_on_speed(speed, max_speed):
+    normalized_speed = speed / max_speed
+    
+    if normalized_speed < 0.53:
+        r = 0
+        g = int(255 * (normalized_speed / 0.33))
+        b = 255
+    elif normalized_speed < 0.76:
+        r = 0
+        g = 255
+        b = int(255 * (1 - (normalized_speed - 0.33) / 0.33))
+    else:
+        r = 255
+        g = int(255 * (1 - (normalized_speed - 0.66) / 0.34))
+        b = 0
+
+    # Ensure RGB values are within the valid range
+    r = max(0, min(255, r))
+    g = max(0, min(255, g))
+    b = max(0, min(255, b))
+    
+    return (r, g, b)
