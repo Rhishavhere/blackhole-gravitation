530b4c3b12b4a3a3f12c6c00d088fb64ad3ef878
Rhishavhere <rhishh.me@gmail.com>
2024-09-09 11:35:08 +0530

    variation changes

--- utils/color.py
+++ utils/color.py
@@ -2,22 +2,18 @@ def get_color_based_on_force(force, max_force):
     normalized_force = force / max_force
     
     if normalized_force < 0.33:
-        # Low force: Blue color
         r = 0
         g = int(255 * (normalized_force / 0.33))
         b = 255
     elif normalized_force < 0.66:
-        # Medium force: Green color
         r = 0
         g = 255
         b = int(255 * (1 - (normalized_force - 0.33) / 0.33))
     else:
-        # High force: Red color
         r = 255
         g = int(255 * (1 - (normalized_force - 0.66) / 0.34))
         b = 0
 
-    # Ensure RGB values are within the valid range
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
@@ -41,7 +37,6 @@ def get_color_based_on_speed(speed, max_speed):
         g = int(255 * (1 - (normalized_speed - 0.66) / 0.34))
         b = 0
 
-    # Ensure RGB values are within the valid range
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
